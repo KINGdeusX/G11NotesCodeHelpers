f7d7b83718e59a9f24d550a16b5c2135f759e9c7
KINGdeusX <warcommelite@gmail.com>
2024-04-07 02:24:34 +0800

    PIO PLATFORM

---
+++ test/ExternalFileDiagnosis.cpp
@@ -0,0 +1,15 @@
+// #include <Arduino.h>
+
+// #define LED 13
+
+// void setup() {
+//     Serial.begin(9600);
+//     pinMode(LED, OUTPUT);
+// }
+// void loop() {
+//     digitalWrite(LED, HIGH);
+//     delay(1000);
+//     Serial.println("Hello World!");
+//     digitalWrite(LED, LOW);
+//     delay(1000);
+// }
