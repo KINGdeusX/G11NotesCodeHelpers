89728929a3b8cfd596aa842cb7dc4f2fa10c103f
TNKLegacy7i0 <jtagas@liceo.edu.ph>
2024-05-08 23:35:50 +0800

    test system added

--- test/ExternalFileDiagnosis.cpp
+++ test/ExternalFileDiagnosis.cpp
@@ -1,15 +1,21 @@
-// #include <Arduino.h>
+#include <Arduino.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
 
-// #define LED 13
+SoftwareSerial bluetoothSerial(10, 11); // RX, TX
 
-// void setup() {
-//     Serial.begin(9600);
-//     pinMode(LED, OUTPUT);
-// }
-// void loop() {
-//     digitalWrite(LED, HIGH);
-//     delay(1000);
-//     Serial.println("Hello World!");
-//     digitalWrite(LED, LOW);
-//     delay(1000);
-// }
+void setup() {
+  bluetoothSerial.begin(115200); // Set the baud rate to match your Bluetooth module
+  Serial.begin(115200);
+}
+
+void loop() {
+  if (bluetoothSerial.available()) { // Check if there is data available to read from Bluetooth
+    char received = bluetoothSerial.read(); // Read the incoming data
+    Serial.print(String(received)); // Concatenate and print the received data to the Serial Monitor
+  }
+  if (Serial.available()) { // Check if there is data available to read from Serial Monitor
+    char toSend = Serial.read(); // Read the incoming data
+    bluetoothSerial.write(toSend); // Send the data to the Bluetooth module
+  }
+}
